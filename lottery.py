a77f111c5d5d90ac23a1ae47defdf4d5e5ceb712
TsungWei Hu <marr.tw@gmail.com>
2020-09-23 16:43:32 +0800

    Add Function Description

--- lottery.py
+++ lottery.py
@@ -1,6 +1,10 @@
 def lot_prize_regular(participant_no, prize_no_array):
+    # randomly pick up a prize_no
+    # fill `assigned` field of the picked_up prize_no with participant_no
     return (participant_no, prize_no)
 
 def lot_prize_special(prize_no, participant_no_array):
+    # randomly pick up a participant_no
+    # fill `assigned` field of prize_no with the picked_up participant_no
     return (winner_no, prize_no)
 
