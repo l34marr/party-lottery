0f9d4e3f287c4fdaa85681f7f49e482ac1d57604
TsungWei Hu <marr.tw@gmail.com>
2020-09-23 14:12:54 +0800

    Repo Initialized

---
+++ lottery.py
@@ -0,0 +1,6 @@
+def lot_prize_regular(participant_no, prize_no_array):
+    return (participant_no, prize_no)
+
+def lot_prize_special(prize_no, participant_no_array):
+    return (winner_no, prize_no)
+
